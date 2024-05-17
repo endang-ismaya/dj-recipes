4e54c18b3649362fc0c847086cc560fe592a6d6e
endangismaya <endang.ismaya@gmail.com>
2024-05-17 18:24:03 +0700

    Section 5: Adding Category Model

--- app__foodie/admin.py
+++ app__foodie/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from app__foodie.models import Category
+
 # Register your models here.
+admin.site.register(Category)
--- app__foodie/models.py
+++ app__foodie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    # date time creation
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return self.name
