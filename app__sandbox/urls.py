80256b74ece3ce3a291448bcea6ddd21ecedd8e3
endangismaya <endang.ismaya@gmail.com>
2024-05-18 18:49:03 +0700

    Section 7: ClassBase DetailView

--- app__sandbox/urls.py
+++ app__sandbox/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from app__sandbox import views
 
 urlpatterns = [
-    path("", views.RecipeListView.as_view(), name="app__sandbox_index")
+    path("", views.RecipeListView.as_view(), name="app__sandbox_index"),
+    path(
+        "<int:pk>/",
+        views.RecipeDetailView.as_view(),
+        name="app__sandbox_recipe_detail",
+    ),
 ]
--- app__sandbox/views.py
+++ app__sandbox/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from app__recipe.models import Recipe
 
@@ -12,3 +12,11 @@ class RecipeListView(ListView):
     model = Recipe
     template_name = "app__sandbox/index.html"
     context_object_name = "recipes"
+
+
+class RecipeDetailView(DetailView):
+    """View the detail of recipe"""
+
+    model = Recipe
+    template_name = "app__sandbox/recipe_detail.html"
+    context_object_name = "recipe"
