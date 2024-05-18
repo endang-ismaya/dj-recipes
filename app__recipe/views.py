f3c95bc840663bee8fda077dd22defa0139e3d96
endangismaya <endang.ismaya@gmail.com>
2024-05-18 13:24:52 +0700

    Section 6: Completed

--- app__recipe/views.py
+++ app__recipe/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+from app__recipe.models import Recipe
+
 
 def recipes(request):
+    recipes = Recipe.objects.all()
+    context = {"recipes": recipes}
     return HttpResponse("Recipe Page")
