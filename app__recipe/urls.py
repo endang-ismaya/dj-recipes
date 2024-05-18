f1a8efeccd1f28b3dc432f5ac2a42c95b0f6bd11
endangismaya <endang.ismaya@gmail.com>
2024-05-18 14:24:00 +0700

    Section 7: Add recipe and recipes urls

--- app__recipe/urls.py
+++ app__recipe/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from app__recipe import views
 
-urlpatterns = [path("", views.recipes, name="app__recipe_index")]
+urlpatterns = [
+    path("", views.recipes, name="app__recipe_index"),
+    path("<int:recipe_id>", views.recipe, name="app__recipe_recipe"),
+]
--- app__recipe/views.py
+++ app__recipe/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from app__recipe.models import Recipe
 
@@ -6,4 +6,10 @@ from app__recipe.models import Recipe
 def recipes(request):
     recipes = Recipe.objects.all()
     context = {"recipes": recipes}
-    return HttpResponse("Recipe Page")
+    return render(request, "app__recipe/recipes.html", context)
+
+
+def recipe(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    context = {"recipe": recipe}
+    return render(request, "app__recipe/recipe.html", context)
